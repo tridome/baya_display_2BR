b72ec0c64b13609b4f0f748ee47e1bdfb28bfb67
tridome <tech@tridomestructures.com>
2025-05-24 20:31:42 -0400

    main files

---
+++ data.js
@@ -0,0 +1,365 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2br_front",
+      "name": "2BR_FRONT",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5642233423091794,
+        "pitch": 0.005403020442599882,
+        "fov": 1.421937074762129
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6058937254262018,
+          "pitch": -0.01062388352157484,
+          "rotation": 0,
+          "target": "1-2br_bedroom"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8500307432199667,
+          "pitch": -0.10812411992276694,
+          "title": "?",
+          "text": "?"
+        },
+        {
+          "yaw": 1.6220802239477727,
+          "pitch": 0.38003904377425,
+          "title": "relax8",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">relax8</span>"
+        }
+      ]
+    },
+    {
+      "id": "1-2br_bedroom",
+      "name": "2BR_BEDROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6574029036041171,
+        "pitch": 0.06318156406444864,
+        "fov": 1.421937074762129
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5484085143097808,
+          "pitch": 0.20338472660645124,
+          "rotation": 0,
+          "target": "2-2br_kitchen"
+        },
+        {
+          "yaw": -1.5402257851530656,
+          "pitch": 0.09568865269226556,
+          "rotation": 0,
+          "target": "0-2br_front"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6658274903576231,
+          "pitch": -0.9047050386127111,
+          "title": "one",
+          "text": "one"
+        },
+        {
+          "yaw": 1.1722522949229823,
+          "pitch": 0.023630431167056543,
+          "title": "two",
+          "text": "two"
+        },
+        {
+          "yaw": 1.992869166709852,
+          "pitch": -0.15600395567148695,
+          "title": "three",
+          "text": "three"
+        },
+        {
+          "yaw": 1.5667581144151823,
+          "pitch": 0.5357631327594312,
+          "title": "four",
+          "text": "four"
+        },
+        {
+          "yaw": 1.8516185964291632,
+          "pitch": 0.16520687125863276,
+          "title": "five",
+          "text": "five"
+        },
+        {
+          "yaw": 0.7801957163597191,
+          "pitch": -0.27136861296826176,
+          "title": "six",
+          "text": "six"
+        },
+        {
+          "yaw": -3.016317406802351,
+          "pitch": 0.39347845925094305,
+          "title": "seven",
+          "text": "seven"
+        },
+        {
+          "yaw": -2.211703895481625,
+          "pitch": -0.22876260564917317,
+          "title": "8",
+          "text": "8"
+        },
+        {
+          "yaw": -1.5672107062376188,
+          "pitch": -0.3512932177212793,
+          "title": "9",
+          "text": "9"
+        },
+        {
+          "yaw": -1.7495384813386714,
+          "pitch": 0.33679870522696476,
+          "title": "10",
+          "text": "10"
+        },
+        {
+          "yaw": -1.0803435225536404,
+          "pitch": 0.1719847644100838,
+          "title": "11",
+          "text": "11"
+        },
+        {
+          "yaw": -2.6909335015833573,
+          "pitch": -0.0077950057258391325,
+          "title": "12",
+          "text": "12"
+        },
+        {
+          "yaw": -1.4361368386690145,
+          "pitch": 0.5136611441856083,
+          "title": "relax2",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">relax2</span>"
+        },
+        {
+          "yaw": -0.36946557245740763,
+          "pitch": 0.7202952335380175,
+          "title": "relax3",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">relax3</span>"
+        },
+        {
+          "yaw": 2.2683201343896524,
+          "pitch": 0.3325702283102654,
+          "title": "relax6",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">relax6</span>"
+        }
+      ]
+    },
+    {
+      "id": "2-2br_kitchen",
+      "name": "2BR_KITCHEN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5766063709966174,
+        "pitch": 0.21513444851795782,
+        "fov": 1.421937074762129
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03206467074737418,
+          "pitch": 0.24800703343447417,
+          "rotation": 0,
+          "target": "3-2-br-bath"
+        },
+        {
+          "yaw": 1.1192229620316851,
+          "pitch": 0.2539248394400744,
+          "rotation": 0,
+          "target": "1-2br_bedroom"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.803296708697177,
+          "pitch": 0.07336526452516523,
+          "title": "one",
+          "text": "one"
+        },
+        {
+          "yaw": 1.394368313586627,
+          "pitch": 0.3939170707184214,
+          "title": "two",
+          "text": "two"
+        },
+        {
+          "yaw": 1.7936297594290824,
+          "pitch": 0.33062284643479245,
+          "title": "three",
+          "text": "three"
+        },
+        {
+          "yaw": 2.2084050254018646,
+          "pitch": 0.3076112993668332,
+          "title": "four",
+          "text": "four"
+        },
+        {
+          "yaw": 2.1233544563276414,
+          "pitch": 0.6229446011997872,
+          "title": "five",
+          "text": "five"
+        },
+        {
+          "yaw": -1.5202371746617285,
+          "pitch": 0.1603673163542929,
+          "title": "100",
+          "text": "100"
+        },
+        {
+          "yaw": 0.5743391339685697,
+          "pitch": 1.0980983743821788,
+          "title": "relax4",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">relax4</span>"
+        },
+        {
+          "yaw": -2.5141756078432724,
+          "pitch": 0.21627304931922886,
+          "title": "relax5",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">relax5</span>"
+        }
+      ]
+    },
+    {
+      "id": "3-2-br-bath",
+      "name": "2 BR-BATH",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6422034618002206,
+        "pitch": 0.30166917797150994,
+        "fov": 1.421937074762129
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.959874524422263,
+          "pitch": 0.34066115950417597,
+          "rotation": 0,
+          "target": "2-2br_kitchen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3684116448366126,
+          "pitch": 0.030082333104420655,
+          "title": "one",
+          "text": "one"
+        },
+        {
+          "yaw": 2.228193903228549,
+          "pitch": -0.817292033806222,
+          "title": "two",
+          "text": "two"
+        },
+        {
+          "yaw": 1.0961883193532653,
+          "pitch": 0.5247919565184489,
+          "title": "three",
+          "text": "three"
+        },
+        {
+          "yaw": 0.6932500092413196,
+          "pitch": 0.9186029295414784,
+          "title": "four",
+          "text": "four"
+        },
+        {
+          "yaw": 2.119217555572762,
+          "pitch": 0.2328490163156225,
+          "title": "five",
+          "text": "five"
+        },
+        {
+          "yaw": 1.5671594836756935,
+          "pitch": 0.3741876808886637,
+          "title": "six",
+          "text": "six"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
